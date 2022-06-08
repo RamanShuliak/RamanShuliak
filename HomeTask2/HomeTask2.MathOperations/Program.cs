d0d68ee2611df27e6fddfccd45509de68d427a2d
RamanShuliak <shuliakrs13@gmail.com>
2022-06-08 13:08:13 +0300

    IndexLast

--- HomeTask2/HomeTask2.MathOperations/Program.cs
+++ HomeTask2/HomeTask2.MathOperations/Program.cs
@@ -4,15 +4,17 @@
     {
         static void Main(string[] args)
         {
-
-            PrintBaseMenu();
+            BaseMenuWithRandom();
         }
 
-        static void PrintBaseMenu()
+        static void BaseMenuWithRandom()
         {
             var numberA = RandomNumberA();
             var numberB = RandomNumberB();
-
+            PrintBaseMenu(numberA, numberB);
+        }
+        static void PrintBaseMenu(int numberA, int numberB)
+        {
             Console.WriteLine($"Now program will generate two numbers from 1 " +
                 $"to 10 for next math operations{Environment.NewLine}" +
                 $"" +
@@ -27,11 +29,11 @@
                 $"5. A ^ B {Environment.NewLine}" +
                 $"6. A! {Environment.NewLine}");
 
-            ChekOperationSelection();
+            ChekOperationSelection(numberA, numberB);
 
         }
 
-        static void ChekOperationSelection()
+        static void ChekOperationSelection(int numberA, int numberB)
         {
             var enterOperation = Console.ReadLine();
             int enterOperationNew = 0;
@@ -41,27 +43,32 @@
                     $"Please, try again{Environment.NewLine}" +
                     $"");
 
-                MenuContinuation();
+                MenuContinuation(numberA, numberB);
             }
             else
             {
-                OperationSelection(enterOperationNew);
+                OperationSelection(enterOperationNew, numberA, numberB);
             }
         }
 
-        static void OperationSelection(int enterOperationNew)
+        static void OperationSelection(int enterOperationNew, int numberA, int numberB)
         {
             switch (enterOperationNew)
             {
                 case 1:
+                    Addition(numberA, numberB);
                     break;
                 case 2:
+                    Subtraction(numberA, numberB);
                     break;
                 case 3:
+                    Multiplication(numberA, numberB);
                     break;
                 case 4:
+                    Dividing(numberA, numberB);
                     break;
                 case 5:
+                    Rising(numberA, numberB);
                     break;
                 case 6:
                     break;
@@ -69,22 +76,22 @@
                     Console.WriteLine($"Operation with such number is not found.{Environment.NewLine}" +
                     $"Please, try again{Environment.NewLine}" +
                     $"");
-                    MenuContinuation();
+                    MenuContinuation(numberA, numberB);
                     break;
             }
         }
 
-        static void MenuContinuation()
+        static void MenuContinuation(int numberA, int numberB)
         {
             Console.WriteLine($"Select, do you wont to continue?{Environment.NewLine}" +
                 $"1. Yes{Environment.NewLine}" +
                 $"2. No{Environment.NewLine}");
 
-            ChekContinuationSelection();
+            ChekContinuationSelection(numberA, numberB);
 
         }
 
-        static void ChekContinuationSelection()
+        static void ChekContinuationSelection(int numberA, int numberB)
         {
             var enterOperation2 = Console.ReadLine();
             int enterOperationNew2 = 0;
@@ -94,20 +101,20 @@
                     $"Please, try again{Environment.NewLine}" +
                     $"");
 
-                MenuContinuation();
+                MenuContinuation(numberA, numberB);
             }
             else
             {
-                ContinuationSelection(enterOperationNew2);
+                ContinuationSelection(enterOperationNew2, numberA, numberB);
             }
         }
 
-        static void ContinuationSelection(int enterOperationNew2)
+        static void ContinuationSelection(int enterOperationNew2, int numberA, int numberB)
         {
             switch (enterOperationNew2)
             {
                 case 1:
-                    PrintBaseMenu();
+                    BaseMenuWithRandom();
                     break;
                 case 2:
                     Console.WriteLine("Program close. Goodby!");
@@ -116,7 +123,7 @@
                     Console.WriteLine($"Operation with such number is not found.{Environment.NewLine}" +
                    $"Please, try again{Environment.NewLine}" +
                    $"");
-                    MenuContinuation();
+                    MenuContinuation(numberA, numberB);
                     break;
             }
         }
@@ -134,7 +141,37 @@
             int nB = random.Next(1, 10);
             return nB;
         }
-        static void Addition()
+        static void Addition(int numberA, int numberB)
+        {
+            Console.WriteLine($"A + B = {numberA + numberB}");
+            MenuContinuation(numberA, numberB);
+        }
+
+        static void Subtraction(int numberA, int numberB)
+        {
+            Console.WriteLine($"A - B = {numberA - numberB}");
+            MenuContinuation(numberA, numberB);
+        }
+
+        static void Multiplication(int numberA, int numberB)
+        {
+            Console.WriteLine($"A * B = {numberA * numberB}");
+            MenuContinuation(numberA, numberB);
+        }
+
+        static void Dividing(int numberA, int numberB)
+        {
+            Console.WriteLine($"A / B = {(double)numberA / numberB}");
+            MenuContinuation(numberA, numberB);
+        }
+
+        static void Rising(int numberA, int numberB)
+        {
+            Console.WriteLine($"A ^ B = {Math.Pow(numberA, numberB)}");
+            MenuContinuation(numberA, numberB);
+        }
+
+        static void Factorial(int numberA, int numberB)
         {
 
         }
