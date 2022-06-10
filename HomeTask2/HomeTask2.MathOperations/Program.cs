860c5a8f07e2992d7ba4f32c51ec0665471ed0d7
RamanShuliak <shuliakrs13@gmail.com>
2022-06-10 11:41:43 +0300

    IndexLast

--- HomeTask2/HomeTask2.MathOperations/Program.cs
+++ HomeTask2/HomeTask2.MathOperations/Program.cs
@@ -15,11 +15,12 @@
         }
         static void PrintBaseMenu(int numberA, int numberB)
         {
-            Console.WriteLine($"Now program will generate two numbers from 1 " +
+            Console.WriteLine($"{Environment.NewLine}" +
+                $"Now program will generate two numbers from 1 " +
                 $"to 10 for next math operations{Environment.NewLine}" +
-                $"" +
-                $"Number A = {numberA},   Number B = {numberB}{Environment.NewLine}" +
                 $"");
+            Console.WriteLine($"Number A = {numberA},   Number B = {numberB}{Environment.NewLine}");
+            Console.WriteLine("");
 
             Console.WriteLine("Alexei, please, select number of math operation (enter 1 - 6)");
             Console.WriteLine($"1. A + B{Environment.NewLine}" +
@@ -39,7 +40,8 @@
             int enterOperationNew = 0;
             if (!int.TryParse(enterOperation, out enterOperationNew))
             {
-                Console.WriteLine($"Entered data is wrong, because include symbols.{Environment.NewLine}" +
+                Console.WriteLine($"{Environment.NewLine}" +
+                    $"Entered data is wrong, because include symbols.{Environment.NewLine}" +
                     $"Please, try again{Environment.NewLine}" +
                     $"");
 
@@ -74,7 +76,8 @@
                     Factorial(numberA, numberB);
                     break;
                 default:
-                    Console.WriteLine($"Operation with such number is not found.{Environment.NewLine}" +
+                    Console.WriteLine($"{Environment.NewLine}" +
+                    $"Operation with such number is not found.{Environment.NewLine}" +
                     $"Please, try again{Environment.NewLine}" +
                     $"");
                     MenuContinuation(numberA, numberB);
@@ -84,7 +87,8 @@
 
         static void MenuContinuation(int numberA, int numberB)
         {
-            Console.WriteLine($"Select, do you wont to continue (enter 1 or 2)?{Environment.NewLine}" +
+            Console.WriteLine($"{Environment.NewLine}" +
+                $"Select, do you wont to continue (enter 1 or 2)?{Environment.NewLine}" +
                 $"1. Yes{Environment.NewLine}" +
                 $"2. No{Environment.NewLine}");
 
@@ -98,6 +102,7 @@
             int enterOperationNew2 = 0;
             if (!int.TryParse(enterOperation2, out enterOperationNew2))
             {
+                Console.WriteLine("");
                 Console.WriteLine($"Entered data is wrong, because include symbols.{Environment.NewLine}" +
                     $"Please, try again{Environment.NewLine}" +
                     $"");
@@ -118,10 +123,12 @@
                     BaseMenuWithRandom();
                     break;
                 case 2:
-                    Console.WriteLine("Program close. Goodby!");
+                    Console.WriteLine($"{Environment.NewLine}" +
+                        $"Program close. Goodby!");
                     break;
                 default:
-                    Console.WriteLine($"Operation with such number is not found.{Environment.NewLine}" +
+                    Console.WriteLine($"{Environment.NewLine}" +
+                        $"Operation with such number is not found.{Environment.NewLine}" +
                    $"Please, try again{Environment.NewLine}" +
                    $"");
                     MenuContinuation(numberA, numberB);
@@ -144,31 +151,40 @@
         }
         static void Addition(int numberA, int numberB)
         {
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"A + B = {numberA + numberB}");
             MenuContinuation(numberA, numberB);
         }
 
         static void Subtraction(int numberA, int numberB)
         {
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"A - B = {numberA - numberB}");
+            Console.WriteLine("");
             MenuContinuation(numberA, numberB);
         }
 
         static void Multiplication(int numberA, int numberB)
         {
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"A * B = {numberA * numberB}");
+            Console.WriteLine("");
             MenuContinuation(numberA, numberB);
         }
 
         static void Dividing(int numberA, int numberB)
         {
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"A / B = {(double)numberA / numberB}");
+            Console.WriteLine("");
             MenuContinuation(numberA, numberB);
         }
 
         static void Rising(int numberA, int numberB)
         {
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"A ^ B = {Math.Pow(numberA, numberB)}");
+            Console.WriteLine("");
             MenuContinuation(numberA, numberB);
         }
 
@@ -186,6 +202,7 @@
                 factorialB *= x;
             }
 
+            Console.WriteLine($"{Environment.NewLine}");
             Console.WriteLine($"Factorial A = {factorialA}{Environment.NewLine}" +
                 $"" +
                 $"Factorial B = {factorialB}{Environment.NewLine}" +
