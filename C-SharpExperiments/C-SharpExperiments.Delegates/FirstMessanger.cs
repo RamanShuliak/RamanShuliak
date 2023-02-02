f049feb735163c366c6ca8a9fd710157163771ba
RamanShuliak <shuliakrs13@gmail.com>
2023-02-02 19:35:40 +0300

    IndexLast

---
+++ C-SharpExperiments/C-SharpExperiments.Delegates/FirstMessanger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpExperiments.Delegates
+{
+    public class FirstMessanger
+    {
+        public void PrintFirstMessange()
+        {
+            Console.WriteLine("Do you want to drink together???");
+        }
+    }
+}
---
+++ C-SharpExperiments/C-SharpExperiments.Delegates/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace C_SharpExperiments.Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstMessanger = new FirstMessanger();
+
+            PrintMessange printMessange;
+
+            printMessange = firstMessanger.PrintFirstMessange;
+
+            printMessange();
+        }
+    }
+}
