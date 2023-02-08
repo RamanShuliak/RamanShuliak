4f0f68ab32820c78548a943a0d3947305747f593
RamanShuliak <shuliakrs13@gmail.com>
2023-02-08 14:28:23 +0300

    IndexLast

---
+++ C-SharpExperiments/C-SharpExperiments.ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C_SharpExperiments.ExtensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var str = "";
+
+            str.IsEmpty();
+
+            string.IsNullOrEmpty(str);
+
+            
+
+
+        }
+    }
+}
---
+++ C-SharpExperiments/C-SharpExperiments.ExtensionMethods/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_SharpExperiments.ExtensionMethods
+{
+    public static class StringExtension
+    {
+        public static bool IsEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str);
+        }
+    }
+}
