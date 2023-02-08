00419ff268a56d91f18512bcf59c2b4872ef2694
RamanShuliak <shuliakrs13@gmail.com>
2023-02-08 13:17:40 +0300

    IndexLast

--- C-SharpExperiments/C-SharpExperiments.Delegates/Program.cs
+++ C-SharpExperiments/C-SharpExperiments.Delegates/Program.cs
@@ -13,7 +13,7 @@
 
             printMessange(10);
 
-            if(printMessange != null)
+            if (printMessange != null)
             {
                 printMessange(10);
             }
@@ -28,6 +28,33 @@
 
                 return messange;
             };
+
+            Console.WriteLine(Environment.NewLine);
+
+            DoSomthing("haha", 15, AddStrAndInt);
+
+            Predicate<string> predicate = st => st.Equals("A");
+
+            Console.WriteLine(predicate("B"));
+
+            var result = DoSomthingWithFunc(5, Operation);
+            Console.WriteLine(result);
+
+            Action<int, int> action = Add;
+
+            action += Add;
+
+
         }
+
+        static void DoSomthing(string x, int y, Action<string, int> action) => action(x, y);
+
+        static int DoSomthingWithFunc(int x, Func<int, int> func) => func(x);
+
+        static int Operation(int x) => x*3;
+
+        static void Add(int x, int y) => Console.WriteLine(x + y);
+
+        static void AddStrAndInt(string x, int y) => Console.WriteLine(x + y);
     }
 }
